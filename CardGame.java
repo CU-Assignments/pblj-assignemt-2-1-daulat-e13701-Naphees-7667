0e12a491b3ca5931522fe36b6b4f6ef4da2ed136
Naphees <naphees6200@gmail.com>
2025-02-28 22:42:59 +0530

    Worksheet 4 - PBLJ

--- CardGame.java
+++ CardGame.java
@@ -4,57 +4,62 @@ class Card {
     String symbol;
     int number;
 
-    public Card(String symbol, int number) {
+    Card(String symbol, int number) {
         this.symbol = symbol;
         this.number = number;
-    }
-
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public int getNumber() {
-        return number;
     }
 }
 
 public class CardGame {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Map<String, List<Card>> cardMap = new TreeMap<>();
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter Number of Cards: ");
-        int n = scanner.nextInt();
+        // Input the number of cards
+        System.out.print("Enter Number of Cards : ");
+        int N = sc.nextInt();
+        sc.nextLine();  // Consume the newline character
 
-        for (int i = 1; i <= n; i++) {
-            System.out.println("Enter card " + i + ":");
-            String symbol = scanner.next();
-            int number = scanner.nextInt();
+        // Map to store cards grouped by symbol
+        Map<String, List<Card>> cardMap = new HashMap<>();
 
+        // Get card details from the user
+        for (int i = 1; i <= N; i++) {
+            System.out.print("Enter card " + i + " symbol: ");
+            String symbol = sc.nextLine();
+            System.out.print("Enter card " + i + " number: ");
+            int number = sc.nextInt();
+            sc.nextLine();  // Consume the newline character
+
+            // Create a new card
+            Card card = new Card(symbol, number);
+
+            // Add card to map, grouped by symbol
             cardMap.putIfAbsent(symbol, new ArrayList<>());
-            cardMap.get(symbol).add(new Card(symbol, number));
+            cardMap.get(symbol).add(card);
         }
 
-        // Print distinct symbols in alphabetical order
-        System.out.println("Distinct Symbols are: ");
-        for (String key : cardMap.keySet()) {
-            System.out.print(key + " ");
+        // Get distinct symbols and sort them alphabetically
+        System.out.println("Distinct Symbols are : ");
+        List<String> symbols = new ArrayList<>(cardMap.keySet());
+        Collections.sort(symbols);
+        for (String symbol : symbols) {
+            System.out.print(symbol + " ");
         }
         System.out.println();
 
-        // Print card details for each symbol
-        for (String symbol : cardMap.keySet()) {
-            List<Card> cards = cardMap.get(symbol);
+        // Print details for each symbol
+        for (String symbol : symbols) {
             System.out.println("Cards in " + symbol + " Symbol");
+            List<Card> cards = cardMap.get(symbol);
             int sum = 0;
             for (Card card : cards) {
-                System.out.println(card.getSymbol() + " " + card.getNumber());
-                sum += card.getNumber();
+                System.out.println(card.symbol + " " + card.number);
+                sum += card.number;
             }
-            System.out.println("Number of cards: " + cards.size());
-            System.out.println("Sum of Numbers: " + sum);
+            System.out.println("Number of cards : " + cards.size());
+            System.out.println("Sum of Numbers : " + sum);
         }
 
-        scanner.close();
+        sc.close();
     }
 }
